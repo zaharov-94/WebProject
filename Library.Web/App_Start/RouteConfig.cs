0f3bd8cb5d39bbc5c1f3d17b7126882e2bc0cd7d
serioga <thetytn@gmail.com>
2018-01-22 23:53:38 +0200

    SomeChanges

--- Library.Web/App_Start/RouteConfig.cs
+++ Library.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Library.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Authorization", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //controller = "Authorization", action = "Login"
             );
         }
     }
