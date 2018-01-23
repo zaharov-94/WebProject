0af8d71ed47b0a0193996d4714143c7fb351f9ae
Anuitex-43 <thetytn@gmail.com>
2018-01-23 12:07:32 +0200

    Change default Route

--- Library.Web/App_Start/RouteConfig.cs
+++ Library.Web/App_Start/RouteConfig.cs
@@ -16,8 +16,7 @@ namespace Library.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                //controller = "Authorization", action = "Login"
+                defaults: new { controller = "Authorization", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
