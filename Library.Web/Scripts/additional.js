8120442204bba0fbe886b7630ecfb4c4421a5062
Anuitex-43 <thetytn@gmail.com>
2018-01-23 18:00:43 +0200

    1

--- Library.Web/Scripts/additional.js
+++ Library.Web/Scripts/additional.js
@@ -2,6 +2,6 @@
 $(window).on('load', function () {
     $("div.sidebar ul li a").click(function () {
         $("div.sidebar ul li").removeClass('active');
-        $(this.parentElement).addClass('active');
+        $(this.parentElement).toggleClass("active");;
     })
 });
