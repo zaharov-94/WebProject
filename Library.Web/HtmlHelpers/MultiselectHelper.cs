9deb829bd2d5a75c77c55573fb30e62208309d94
serioga <thetytn@gmail.com>
2018-01-29 23:15:26 +0200

    h

---
+++ Library.Web/HtmlHelpers/MultiselectHelper.cs
@@ -0,0 +1,15 @@
+﻿using Library.Web.Entities;
+using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Library.Web.HtmlHelpers
+{
+    public static class MultiselectHelper
+    {
+        public static MvcHtmlString MultiselectFor(this HtmlHelper<Book> htmlHelper, Expression<Func<Book, PublicationHouse>> expression, object htmlAttributes)
+        {
+
+        }
+    }
+}
