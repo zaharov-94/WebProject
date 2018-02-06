b3b9a3cccc78cc9fc1b8b15ba0d4e596172c21cb
Anuitex-43 <thetytn@gmail.com>
2018-02-06 14:14:47 +0200

    2

---
+++ Library.Web/App_Start/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Library.Web.App_Start.Startup))]
+
+namespace Library.Web.App_Start
+{
+    public class Startup
+    {
+        //IServiceCreator serviceCreator = new ServiceCreator();
+        public void Configuration(IAppBuilder app)
+        {
+            //app.CreatePerOwinContext<IUserService>(CreateUserService);
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            //    LoginPath = new PathString("/Account/Login"),
+            //});
+        }
+
+        //private IUserService CreateUserService()
+        //{
+        //    return serviceCreator.CreateUserService("DefaultConnection");
+        //}
+    }
+}
--- Library.Web/Controllers/BookController.cs
+++ Library.Web/Controllers/BookController.cs
@@ -10,7 +10,6 @@ using System.Web.Mvc;
 
 namespace Library.Web.Controllers
 {
-    [Authorize]
     public class BookController : Controller
     {
         private BookService _bookService;
