6ad030c21a542d3ee79df2448039c4cf04354e64
serioga <thetytn@gmail.com>
2018-02-06 21:37:05 +0200

    1

--- Library.Web/App_Start/RouteConfig.cs
+++ Library.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Library.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Book", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
